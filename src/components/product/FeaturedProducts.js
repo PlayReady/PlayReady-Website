86bdc145e00b45d4fc713e1a9899c19aa7044fd5
ENIGMA <stanborst@gmail.com>
2023-10-05 00:46:54 +0200

    added structure for featured products

---
+++ src/components/product/FeaturedProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FeaturedProducts() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default FeaturedProducts;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import "./Home.css"
 import background from "../../assets/placeholders/computers/homepage-background.png"
 import WelcomeSection from "./WelcomeSection";
+import FeaturedProducts from "../../components/product/FeaturedProducts";
 
 function Home() {
+
     return (
         <div className="main">
             <WelcomeSection/>
+            <FeaturedProducts/>
         </div>
     );
 }
