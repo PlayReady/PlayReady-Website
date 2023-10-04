d58753e562dc75e2f48802f93cad4150b63282d7
ENIGMA <stanborst@gmail.com>
2023-10-04 22:24:03 +0200

    added routes and home page route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <>
+    return (
+        <>
+            <Header/>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home/>}
+                />
+            </Routes>
 
-    </>
-  );
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../../components/Header/Header";
+import logosvg from "../../assets/logo white.svg";
+
+function Home(props) {
+    return (
+        <>
+            heLLO?
+        </>
+    );
+}
+
+export default Home;
