fb9001125b16799dfef27d1e2ac4b417e9fff423
ENIGMA <stanborst@gmail.com>
2023-10-05 01:57:07 +0200

    added featured products to homepage

--- src/components/product/FeaturedProducts.js
+++ src/components/product/FeaturedProducts.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import "./Product.css";
+import ProductCard from "./ProductCard";
+import productImage from "../../assets/placeholders/computers/product.png"
 
 function FeaturedProducts() {
-    return (
-        <div>
 
+    const featuredProducts=[
+        {
+            image:productImage,
+            name:"Game pc",
+            price:30,
+        },
+        {
+            image:productImage,
+            name:"Game pc",
+            price:30,
+        },
+        {
+            image:productImage,
+            name:"Game pc",
+            price:30,
+        },
+    ]
+    return (
+        <div className="productFeature">
+            {featuredProducts.map((product)=>{
+                return <ProductCard product={product}/>
+            })}
         </div>
     );
 }
--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import "./Product.css";
 
-function ProductCard() {
+function ProductCard({product}) {
     return (
-        <div>
-
+        <div className="productCard">
+            <img src={product.image} alt="product" className="productImage"/>
+            <div className="productCardContent">
+                <h1>{product.name}</h1>
+                <p>€{product.price},- per maand</p>
+                <button>Read more</button>
+            </div>
         </div>
     );
 }
