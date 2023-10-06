88ce0756c6b0fed711d3bc7452efeca695df4b6c
ENIGMA <stanborst@gmail.com>
2023-10-06 05:43:43 +0200

    replaced hardcoded image with image from backend

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Product.css';
 import Button from '../button/Button';
-import productImage from '../../assets/placeholders/computers/product.png';
 
 function ProductCard({product}) {
   return (
     <div className="productCard">
-      <img src={productImage} alt="product" className="productImage"/>
+      <img
+        src={`data:image/png;base64,${product.image}`}
+        alt="product"
+        className="productImage"
+      />
       <div className="productCardContent">
         <h1>{product.name}</h1>
         <p>€{product.price},- per maand</p>
