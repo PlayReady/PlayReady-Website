e74e5a9e62ee4806b9e17d78fa7c22c5d414675c
ENIGMA <stanborst@gmail.com>
2023-10-07 18:05:15 +0200

    removed link functionality for products

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,14 +1,10 @@
 import React, {useState} from 'react';
 import './Product.css';
 import Button from '../button/Button';
-import {useNavigate} from 'react-router-dom';
 
 function ProductCard({product}) {
-  const navigation=useNavigate();
   const [Loading, setLoading] = useState(false);
   function handleClick() {
-    setLoading(true);
-    navigation('/products/'+product.id);
   }
   return (
     <div className="productCard">
@@ -20,7 +16,7 @@ function ProductCard({product}) {
       <div className="productCardContent">
         <h1>{product.name}</h1>
         <p>€{product.price},- per maand</p>
-        <Button onclick={handleClick} loading={Loading}>Lees meer</Button>
+        <Button onclick={handleClick} loading={Loading}>Vraag aan</Button>
       </div>
     </div>
   );
