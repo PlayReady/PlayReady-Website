1d67f092c564067f0fad5a0859b6cc2bb3a54588
ENIGMA <stanborst@gmail.com>
2023-10-09 15:17:57 +0200

    added confirmed state to button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import './Button.css';
 import Loader from '../loader/Loader';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheck} from '@fortawesome/free-solid-svg-icons';
 
-function Button({children, onclick, loading, disabled}) {
+function Button({children, onclick, loading, disabled, confirmed}) {
   function handleClick() {
     if (!disabled) {
       onclick();
     }
   }
+
   return (
     <button
       onClick={handleClick}
-      className={disabled && 'disabled'}
+      className={
+        disabled ? 'disabled' :
+        loading ? 'loading' :
+        confirmed && 'confirmed'
+      }
     >
       {loading && <div className={`overlay`}><Loader/></div>}
-      <div className={loading && 'loading'}>{children}</div>
+      {confirmed && <div className={`overlay`}>
+        <FontAwesomeIcon icon={faCheck}/>
+      </div>}
+      <div>
+        {children}
+      </div>
     </button>
   );
 }
