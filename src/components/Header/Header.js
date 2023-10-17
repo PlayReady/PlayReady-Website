b982747bf1ae44fd709d8ec5a11f9ac1c8d11aa6
ENIGMA <stanborst@gmail.com>
2023-10-17 02:01:44 +0200

    added ADMIN and RENTER options in the nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,46 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import logosvg from '../../assets/logo white.svg';
 import './Header.css';
 import Nav from '../Nav/Nav';
 import {AuthContext} from '../../context/AuthContext';
 
 function Header() {
-  const {isAuth}=useContext(AuthContext);
+  const {isAuth, getUserInfo}=useContext(AuthContext);
+  const [roles, setRoles] = useState([]);
 
+  async function fetchUserInfo() {
+    try {
+      const userInfo = await getUserInfo();
+      setRoles(userInfo.roles);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(()=>{
+    if (isAuth) {
+      fetchUserInfo();
+    }
+  }, [isAuth]);
   const leftOptions = [
     {name: 'Home', path: '/'},
     {name: 'Products', path: '/products'},
-  ];
-  const rightOptionsAuth = [
-    {name: 'Profiel', path: '/profile'},
-    {name: 'Log uit', path: '/logout'},
   ];
   const rightOptions = [
     {name: 'Log in', path: '/login'},
     {name: 'Registreer', path: '/register'},
   ];
+  const rightOptionsAuth = [
+    {name: 'Profiel', path: '/profile'},
+    {name: 'Log uit', path: '/logout'},
+  ];
+  if (isAuth && (roles.includes('ROLE_ADMIN')||roles.includes('ROLE_RENTER'))) {
+    rightOptionsAuth.unshift(
+        {name: 'facturen', path: '/invoices'},
+        {name: 'contracten', path: '/contracts'},
+    );
+  }
+
   return (
     <div className="Header">
       <div className="nav">
